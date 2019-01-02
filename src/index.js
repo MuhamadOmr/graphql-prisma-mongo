198560c154490e187d651d00dff921e7e4afc356
Muhamad Omar <mohamadomr33@gmail.com>
2019-01-02 18:53:59 +0200

    [update] add linting for javascript and pre commits

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+// require('dotenv').config({
+//   path: 'variables.env'
+// });
+// const createServer = require('./createServer');
+// const db = require('./db');
 
-const server = createServer();
+// const server = createServer();
 
-server.express.use(cookieParser());
-// TODO Use express middlware to populate current user
+// server.express.use(cookieParser());
+// // TODO Use express middlware to populate current user
 
-server.start(
-  {
-    cors: {
-      credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
-  },
-  deets => {
-    console.log(`Server is now running on port http://localhost:${deets.port}`);
-  }
-);
+// server.start({
+//     cors: {
+//       credentials: true,
+//       origin: process.env.FRONTEND_URL,
+//     },
+//   },
+//   deets => {
+//     console.log(`Server is now running on port http://localhost:${+deets.port}`);
+//   }
+// );
